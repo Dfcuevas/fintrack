cbb84b6b8524bc0a85620e7c9cca44aa64c69f1a
Diego Cuevas <ingdcuevas@gmail.com>
2026-06-29 09:01:13 -0500

    login styles

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <section className="flex items-center justify-center min-h-screen p-4 gap-4">
+      <div className="flex items-center justify-center">
+        <SignIn />
+      </div>
+      <div className="hidden md:block relative w-[257px] h-[508px] rounded-3xl overflow-hidden">
+        <Image
+          className="object-cover"
+          src="/login-left-image.png"
+          fill
+          loading="eager"
+          alt="Login left image"
+        />
+      </div>
+    </section>
+  );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <section className="flex items-center justify-center min-h-screen p-4 gap-4">
+      <div className="hidden md:block relative w-[300px] h-[556px] rounded-3xl overflow-hidden">
+        <Image
+          className="object-cover"
+          src="/bg-right-signup.png"
+          fill
+          loading="eager"
+          alt="Login left image"
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <SignUp />
+      </div>
+    </section>
+  );
 }
