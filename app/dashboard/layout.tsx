c9a66e9378be308b8dd4c0c6f48493825ba7816a
Diego Cuevas <ingdcuevas@gmail.com>
2026-07-21 16:05:19 -0500

    doing some stuffs

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="flex h-screen">
+    {/* Columna izquierda - sidebar fijo */}
+    <aside className="w-64 shrink-0 border-r border-stroke">
+
+    </aside>
+
+    {/* Columna derecha - contenido dinámico */}
+    <main className="flex-1 overflow-y-auto">
+      {children}
+    </main>
+  </div>;
+}
---
+++ components/layout/DashboardSidebar.tsx
@@ -0,0 +1,7 @@
+export const DashboardSidebar = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
